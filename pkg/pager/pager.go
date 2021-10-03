8465d759b634d077d400d2e6e1b204d79d91a1d5
YottaLee <849299796@qq.com>
2021-10-04 06:33:33 +0800

    pager: fix return err

--- pkg/pager/pager.go
+++ pkg/pager/pager.go
@@ -177,9 +177,12 @@ func (pager *Pager) GetPage(pagenum int64) (page *Page, err error) {
 		fmt.Printf("not in pageTable %d\n", pagenum)
 		curPage, err = pager.NewPage(pagenum)
 		if err != nil {
-			fmt.Printf("reach return1 \n")
-			//return nil, errors.New("no unpinned page available")
-			return nil, err
+			fmt.Printf("reach err1 \n")
+			err = pager.ReadPageFromDisk(curPage, pagenum)
+			if err != nil {
+				fmt.Printf("reach err2 \n")
+				return nil, err
+			}
 		}
 		fmt.Printf("not reach return1 \n")
 		err = pager.ReadPageFromDisk(curPage, pagenum)
