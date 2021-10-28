43092abdc8aa4c1f9f15c4d9b54a678f0881d64b
YottaLee <849299796@qq.com>
2021-10-28 10:59:06 +0800

    hash: fix runtime error, split before insert

--- pkg/hash/bucket.go
+++ pkg/hash/bucket.go
@@ -9,14 +9,14 @@ import (
 	utils "github.com/brown-csci1270/db/pkg/utils"
 )
 
-// HashBucket HashBucket.
+// HashBucket.
 type HashBucket struct {
 	depth   int64
 	numKeys int64
 	page    *pager.Page
 }
 
-// NewHashBucket Construct a new HashBucket.
+// Construct a new HashBucket.
 func NewHashBucket(pager *pager.Pager, depth int64) (*HashBucket, error) {
 	newPN := pager.GetFreePN()
 	newPage, err := pager.GetPage(newPN)
@@ -28,94 +28,96 @@ func NewHashBucket(pager *pager.Pager, depth int64) (*HashBucket, error) {
 	return bucket, nil
 }
 
-// GetDepth Get local depth.
+// Get local depth.
 func (bucket *HashBucket) GetDepth() int64 {
 	return bucket.depth
 }
 
-// GetPage Get a bucket's page.
+// Get a bucket's page.
 func (bucket *HashBucket) GetPage() *pager.Page {
 	return bucket.page
 }
 
-// Find Finds the entry with the given key.
+// Finds the entry with the given key.
 func (bucket *HashBucket) Find(key int64) (utils.Entry, bool) {
-	for i := int64(0); i < bucket.numKeys; i++ {
-		if bucket.getKeyAt(i) == key {
-			return bucket.getCell(i), true
+	//panic("function not yet implemented");
+	var idx int64
+	for i := 0; i < int(bucket.numKeys); i++ {
+		if bucket.getKeyAt(int64(i)) == key {
+			idx = int64(i)
+			return bucket.getCell(idx), true
 		}
 	}
+
 	return nil, false
 }
 
-// Insert Inserts the given key-value pair, splits if necessary.
+// Inserts the given key-value pair, splits if necessary.
 func (bucket *HashBucket) Insert(key int64, value int64) (bool, error) {
+	//panic("function not yet implemented")
 	if bucket.numKeys == BUCKETSIZE {
 		return true, nil
 	}
-
-	// append the entry and increase the numKeys by one
 	bucket.updateKeyAt(bucket.numKeys, key)
 	bucket.updateValueAt(bucket.numKeys, value)
 	bucket.updateNumKeys(bucket.numKeys + 1)
-
-	// the bucket overflows, notify the hash table
 	return false, nil
 }
 
 // Update the given key-value pair, should never split.
 func (bucket *HashBucket) Update(key int64, value int64) error {
-	// search for the entry to update
-	for i := int64(0); i < bucket.numKeys; i++ {
-		if bucket.getKeyAt(i) == key {
-			bucket.updateValueAt(i, value)
+	//panic("function not yet implemented");
+	var idx int64
+	for i := 0; i < int(bucket.numKeys); i++ {
+		if bucket.getKeyAt(int64(i)) == key {
+			idx = int64(i)
+			bucket.updateValueAt(idx, value)
 			return nil
 		}
 	}
-	// if not found, return key not found error
-	return errors.New("update error: key not found")
+
+	return errors.New("Can not update nonexistent key value")
+
 }
 
 // Delete the given key-value pair, does not coalesce.
 func (bucket *HashBucket) Delete(key int64) error {
-	deletionIdx := int64(-1)
-	// search for the entry to delete
-	for i := int64(0); i < bucket.numKeys; i++ {
-		if bucket.getKeyAt(i) == key {
-			deletionIdx = i
+	//panic("function not yet implemented");
+	idx := int64(-1)
+	for i := 0; i < int(bucket.numKeys); i++ {
+		if bucket.getKeyAt(int64(i)) == key {
+			idx = int64(i)
 			break
 		}
 	}
 
-	// if not found, return key not found error
-	if deletionIdx == -1 {
-		return errors.New("delete error: key not found")
+	if idx == -1 {
+		fmt.Printf("err, index: %d key: %d\n", idx, key)
+
+		return errors.New("Can not delete nonexistent key")
 	}
 
-	// if found, shift all the key from deletionIdx + 1 to numKeys - 1 to left by one
-	for i := deletionIdx + 1; i < bucket.numKeys; i++ {
+	for i := idx + 1; i < bucket.numKeys; i++ {
 		bucket.updateKeyAt(i-1, bucket.getKeyAt(i))
 		bucket.updateValueAt(i-1, bucket.getValueAt(i))
-	}
 
-	// decrease numKeys by one
+	}
 	bucket.updateNumKeys(bucket.numKeys - 1)
 	return nil
 }
 
 // Select all entries in this bucket.
 func (bucket *HashBucket) Select() ([]utils.Entry, error) {
-	ret := make([]utils.Entry, 0)
-
-	if ret == nil {
+	//panic("function not yet implemented");
+	entrylist := make([]utils.Entry, 0)
+	if entrylist == nil {
 		return nil, errors.New("select error: internal error")
 	}
-
-	// append all the entries
-	for i := int64(0); i < bucket.numKeys; i++ {
-		ret = append(ret, bucket.getCell(i))
+	for i := 0; i < int(bucket.numKeys); i++ {
+		entry := bucket.getCell(int64(i))
+		entrylist = append(entrylist, entry)
 	}
-	return ret, nil
+	return entrylist, nil
 }
 
 // Pretty-print this bucket.
